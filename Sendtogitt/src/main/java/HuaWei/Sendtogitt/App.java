9b221f09a05820a82b61a225c0c4e4380ea0bd6e
huawei <1697205717@qq.com>
2018-12-17 19:59:02 +0800

    aaa

---
+++ Sendtogitt/src/main/java/HuaWei/Sendtogitt/App.java
@@ -0,0 +1,13 @@
+package HuaWei.Sendtogitt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
